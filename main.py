4877250c8772057eaaf9191f00e4cbbc0efd629e
kira-sakuya <kira.sakuya.sk@gmail.com>
2024-09-19 12:47:15 +0200

    création fichier player.py et circleshape.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+import player
 
 def main():
     #print("Starting asteroids!")
@@ -11,13 +12,18 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    ply = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        ply.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
